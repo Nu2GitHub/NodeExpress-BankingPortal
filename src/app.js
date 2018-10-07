79ea995e6a3c14363e65325b3fcb0237e77c54f5
Andy <andrew.king198203@gmail.com>
2018-10-07 20:46:40 +0100

    Completed the module!

--- src/app.js
+++ src/app.js
@@ -49,8 +49,8 @@ app.get('/transfer', (req, res) => {
 app.post('/transfer', (req, res) => {
     accounts[req.body.from].balance = accounts[req.body.from].balance - req.body.amount;
     accounts[req.body.to].balance = parseInt(accounts[req.body.to].balance) + parseInt(req.body.amount,10);
-    const accountJSON = JSON.stringify(accounts, null, 4);
-    fs.writeFileSync(path.join(__dirname,'json/accounts.json'), accountJSON, 'utf8');
+    const accountsJSON = JSON.stringify(accounts, null, 4);
+    fs.writeFileSync(path.join(__dirname,'json/accounts.json'), accountsJSON, 'utf8');
     res.render('transfer', { message: 'Transfer Completed' });
 });
 
@@ -60,7 +60,7 @@ app.get('/payment', (req, res) => {
 
 app.post('/payment', (req, res) => {
     accounts.credit.balance -= req.body.amount;
-    accounts.credit.avaliable += parseInt(req.body.amount, 10);
+    accounts.credit.available += parseInt(req.body.amount, 10);
     const accountsJSON = JSON.stringify(accounts, null, 4);
     fs.writeFileSync(path.join(__dirname, 'json', 'accounts.json'), accountsJSON, 'utf8');
     res.render('payment', { message: 'Payment Succesful', account: accounts.credit });
